5d5ae14ee833e68f4c16bac8e92b411caa2721da
AnthropoidFHJ <f.h.jibon1250@gmail.com>
2025-12-06 04:26:10 +0600

    Error Fixed

--- app/schemas/schema.py
+++ app/schemas/schema.py
@@ -5,7 +5,6 @@ class UserCreate(BaseModel):
     name: str = Field(min_length=2, max_length=50)
     email: EmailStr
     password: str = Field(min_length=8, max_length=72)
-    confirm_password: str = Field(min_length=8, max_length=72)
 
 class UserRead(BaseModel):
     id: int
